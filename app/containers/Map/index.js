8c1127e8d9771e6f4d5da9962f16eaebec178e51
Alex Mejias <contact@lazers.tv>
2016-08-26 10:06:39 -0500

    Added Map component skeleton.

---
+++ app/containers/Map/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import styles from './styles.css'
+
+export class index extends React.Component {
+  static propTypes = {
+    name: React.PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+// Implement map dispatch to props
+)(index)
